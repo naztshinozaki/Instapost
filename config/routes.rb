a6b65c5d6f8bafe490acbfb1ddd3ab1d7af9359d
Shinozaki Robyn Natsuko <shinozakirobynnatsuko@Shinozakis-MacBook-Air.local>
2016-07-20 13:23:47 +0545

    styling everything

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,4 +59,5 @@ Rails.application.routes.draw do
   resources :posts do 
     resources :comments
   end 
+  resources :users, only: :show
 end
